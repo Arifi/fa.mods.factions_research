89d9be1d3c2882d93b576fad99c814b1fedf1745
mourad <mourad.arifi@armediacg.com>
2016-10-24 20:10:02 +0100

    Initial Commit

---
+++ mod_info.lua
@@ -0,0 +1,16 @@
+name = "Factions Research"
+uid = "7c0dce67-cff6-4343-807b-28b1b48bfda5"
+version = 1
+copyright = "Gurzil"
+description = "Add Upgrade to ACU allowing them build T1 land factory of a specific faction"
+author = "Gurzil"
+icon = ""
+selectable = true
+enabled = true
+exclusive = false
+ui_only = false
+requires = {}
+requiresNames = {}
+conflicts = {}
+before = {}
+after = {}
